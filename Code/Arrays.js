a623104e8aadbd5d5e5145e30a86dc7f3e8ca7af
hxjini <s24034@gsm.hs.kr>
2024-08-13 20:32:00 +0900

    Arrays

---
+++ Code/Arrays.js
@@ -0,0 +1,9 @@
+const daysOfWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more day
+daysOfWeek.push('sun');
+
+console.log(daysOfWeek);
