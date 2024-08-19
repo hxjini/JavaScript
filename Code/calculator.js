eff32069509e77a00e250c583795fd216a0a7016
hxjini <s24034@gsm.hs.kr>
2024-08-19 18:06:28 +0900

    calculator

---
+++ Code/calculator.js
@@ -0,0 +1,23 @@
+const calculator = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+  times: function (a, b) {
+    console.log(a * b);
+  },
+  divide: function (a, b) {
+    console.log(a / b);
+  },
+  power: function (a, b) {
+    console.log(a ** b);
+  },
+};
+
+calculator.plus(3, 2);
+calculator.minus(5, 2);
+calculator.times(2, 2);
+calculator.divide(8, 2);
+calculator.power(4, 2);
